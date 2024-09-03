2d6dd9ec510b9cb56c15e7338af1a281948bb828
Gaya39877 <rasangikahw@gmail.com>
2024-09-03 16:51:51 +0530

    update

--- query.py
+++ query.py
@@ -1,48 +1,23 @@
-# import mysql.connector
-# import streamlit as st
-
-# try:
-#     # Connection
-#     conn = mysql.connector.connect(
-#         host='localhost',
-#         port='3306',
-#         user='root',
-#         passwd='',
-#         db='mydb'
-#     )
-#     c = conn.cursor()
-
-#     # fetch
-#     def view_all_data():
-#         c.execute('SELECT * FROM insurance ORDER BY id ASC')
-#         data = c.fetchall()
-#         return data
-
-# except mysql.connector.Error as err:
-#     st.error(f"Error: {err}")
-
 import mysql.connector
+import streamlit as st
 
-def view_all_data():
-    try:
-        # Connection
-        conn = mysql.connector.connect(
-            host='localhost',
-            port='3306',
-            user='root',
-            passwd='',
-            db='mydb'
-        )
-        c = conn.cursor()
+try:
+    # Connection
+    conn = mysql.connector.connect(
+        host='localhost',
+        port='3306',
+        user='root',
+        passwd='',
+        db='mydb'
+    )
+    c = conn.cursor()
 
-        # Fetch
+    # fetch
+    def view_all_data():
         c.execute('SELECT * FROM insurance ORDER BY id ASC')
         data = c.fetchall()
         return data
 
-    except mysql.connector.Error as err:
-        print(f"Error: {err}")
-        return []
-
-
+except mysql.connector.Error as err:
+    st.error(f"Error: {err}")
 
