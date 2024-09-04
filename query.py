f56b71ed6762aff0baff7b8b6f60f47867372451
Gaya39877 <rasangikahw@gmail.com>
2024-09-04 22:02:35 +0530

    updated

--- query.py
+++ query.py
@@ -1,3 +1,26 @@
+# import mysql.connector
+# import streamlit as st
+
+# try:
+#     # Connection
+#     conn = mysql.connector.connect(
+#         host='localhost',
+#         port='3306',
+#         user='root',
+#         passwd='',
+#         db='mydb'
+#     )
+#     c = conn.cursor()
+
+#     # fetch
+#     def view_all_data():
+#         c.execute('SELECT * FROM insurance ORDER BY id ASC')
+#         data = c.fetchall()
+#         return data
+
+# except mysql.connector.Error as err:
+#     st.error(f"Error: {err}")
+
 import mysql.connector
 import streamlit as st
 
