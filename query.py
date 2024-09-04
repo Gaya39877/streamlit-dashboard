93a0d8a6ff0d4cb73f4244747765a7a2e969eccc
Gaya39877 <rasangikahw@gmail.com>
2024-09-04 22:56:56 +0530

    updated

--- query.py
+++ query.py
@@ -1,47 +1,47 @@
-import mysql.connector
-import streamlit as st
-
-try:
-    # Connection
-    conn = mysql.connector.connect(
-        host='localhost',
-        port='3306',
-        user='root',
-        passwd='',
-        db='mydb'
-    )
-    c = conn.cursor()
-
-    # fetch
-    def view_all_data():
-        c.execute('SELECT * FROM insurance ORDER BY id ASC')
-        data = c.fetchall()
-        return data
-
-except mysql.connector.Error as err:
-    st.error(f"Error: {err}")
-
 # import mysql.connector
+# import streamlit as st
 
-# # Function to fetch data
-# def view_all_data():
-#     try:
-#         # Connection
-#         conn = mysql.connector.connect(
-#             host='localhost',
-#             port='3306',
-#             user='root',
-#             passwd='',
-#             db='mydb'
-#         )
-#         c = conn.cursor()
+# try:
+#     # Connection
+#     conn = mysql.connector.connect(
+#         host='localhost',
+#         port='3306',
+#         user='root',
+#         passwd='',
+#         db='mydb'
+#     )
+#     c = conn.cursor()
 
-#         # fetch
+#     # fetch
+#     def view_all_data():
 #         c.execute('SELECT * FROM insurance ORDER BY id ASC')
 #         data = c.fetchall()
 #         return data
 
-#     except mysql.connector.Error as err:
-#         print(f"Error: {err}")
+# except mysql.connector.Error as err:
+#     st.error(f"Error: {err}")
+
+import mysql.connector
+
+# Function to fetch data
+def view_all_data():
+    try:
+        # Connection
+        conn = mysql.connector.connect(
+            host='localhost',
+            port='3306',
+            user='root',
+            passwd='',
+            db='mydb'
+        )
+        c = conn.cursor()
+
+        # fetch
+        c.execute('SELECT * FROM insurance ORDER BY id ASC')
+        data = c.fetchall()
+        return data
+
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
 
 
